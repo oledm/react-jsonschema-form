65d0ad18895cabf16fc995b0595ab974b0fb2f01
Oleynik Dmitry <oleynik@adwz.ru>
2017-05-22 12:56:14 +0300

    Datepicker customization

--- src/components/widgets/DateTimeInputWidget.js
+++ src/components/widgets/DateTimeInputWidget.js
@@ -5,7 +5,7 @@ import 'moment/locale/ru'
 import 'react-datetime/css/react-datetime.css'
 
 export const formatDateCustom = (format) => (date) => date
-  ? moment(date).format(format)
+  ? moment(new Date(date)).format(format)
   : undefined
 export const formatDate = formatDateCustom('DD.MM.YYYY HH:mm')
 
@@ -27,57 +27,25 @@ function DateTimeInputWidget(props) {
     ...inputProps
   } = props;
 
-//      value={value ? value : moment()}
-//      onChange={(event) => {
-//        onChange(formatDate(event))
-//      }}
-//      value={value ? value : moment()}
-
-//    <Datetime
-//      locale="ru"
-//      dateFormat='DD.MM.YYYY'
-//      timeFormat='HH:mm'
-//      placeholder='Выберите дату'
-//      timeConstraints={{
-//        minutes: {
-//          step: 5,
-//        }
-//      }}
-//      inputProps={
-//        formContext && formContext.preview ?
-//        {
-//          className: 'ant-input ant-input-lg',
-//        }
-//        : null
-//      }
-//      viewMode='days'
-//      value={value}
-//      onChange={(event) => {
-//        const res = formatDate(event)
-//        console.log('res', res)
-//        onChange(res)
-//      }}
-
-//      value={value}
-//      onChange={(value) => {
-//        const res = formatDate(value)
-//        console.log('res', res)
-//
-//        onChange(value)
-//      }}
-//      isValidDate={isValid}
-//      value={moment(value, 'DD.MM.YYYY HH:mm')}
-//      value={value}
   return (
     <Datetime
+      locale="ru"
       dateFormat='DD.MM.YYYY'
       timeFormat='HH:mm'
-      closeOnSelect
+      timeConstraints={{
+        minutes: {
+          step: 5,
+        }
+      }}
+      inputProps={
+        {
+          className: formContext && formContext.preview ? 'ant-input ant-input-lg' : 'form-control',
+          placeholder: 'ДД.ММ.ГГГГ чч:мм'
+        }
+      }
+      isValidDate={isValid}
       onChange={(value) => {
-//        console.log('value', value)
-        const res = formatDate(value)
-//        console.log('res', res)
-        onChange(res)
+        onChange(formatDate(value))
       }}
     />
   )
